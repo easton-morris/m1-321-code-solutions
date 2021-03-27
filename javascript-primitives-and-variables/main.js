59612dcef7fc827bb3442ca8ddfe17356650815f
Easton Morris <eastonmorris9@gmail.com>
2021-03-27 10:45:34 -0700

    javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Easton Morris';
+var isCool = false;
+var totalPets = 1;
+
+console.log('Full Name:', fullName);
+console.log('Cool?', isCool);
+console.log('Total Number of Pets:', totalPets);
+
+console.log('Full Name type:', typeof fullName);
+console.log('isCool type:', typeof isCool);
+console.log('totalPets type:', typeof totalPets);
+
+var unknown = null;
+console.log('Unknown:', unknown);
+console.log('Unknown type:', typeof unknown);
+
+var never;
+console.log('Never:', never);
+console.log('Never type:', typeof never);
