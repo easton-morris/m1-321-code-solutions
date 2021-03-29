85b4610585f518a6f97beef61e8aa0fbfd4e8eea
Easton Morris <eastonmorris9@gmail.com>
2021-03-28 20:29:28 -0700

    finished javascript-objects

--- javascript-objects/main.js
+++ javascript-objects/main.js
@@ -13,3 +13,26 @@ student.previousOccupation = 'IT';
 
 console.log('Lives in Irvine?', student.livesInIrvine);
 console.log('Previous Occupation:', student.previousOccupation);
+
+var vehicle = {
+  make: 'Ford',
+  model: 'Mustang',
+  year: 1967
+};
+
+vehicle['color'] = 'blue';
+vehicle['isConvertible'] = false;
+
+console.log('Color:', vehicle['color']);
+console.log('Is it convertible?', vehicle['isConvertible']);
+console.log('Full Vehicle:', vehicle);
+
+var pet = {
+  name: 'Mercy',
+  type: 'White Swiss Shepherd'
+};
+
+delete pet.name;
+delete pet.type;
+
+console.log('Pet:', pet);
