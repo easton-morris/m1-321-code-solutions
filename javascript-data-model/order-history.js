7a251eb8c2e7c92d2e970294886cac5e4308c6f9
Easton Morris <eastonmorris9@gmail.com>
2021-04-01 21:59:20 -0700

    javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,130 @@
+var orderHistory = [
+  {
+    orderDate: {
+      day: 4,
+      month: 8,
+      year: 2020
+    },
+    orderCost: 34,
+    userName: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    deliveryDate: {
+      day: 8,
+      month: 8,
+      year: 2020
+    },
+    items: [
+      {
+        itemImage: 'www.url.com/someimg',
+        itemName: 'JavaScript for impatient programmers',
+        itemAuthor: 'Rauschmayer, Dr. Axel',
+        returnLastDate: {
+          day: 7,
+          month: 9,
+          year: 2020
+        },
+        price: 31.55
+      }
+    ],
+    productSup: false
+  },
+
+  {
+    orderDate: {
+      day: 19,
+      month: 7,
+      year: 2020
+    },
+    orderCost: 44.53,
+    userName: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    deliveryDate: {
+      day: 20,
+      month: 7,
+      year: 2020
+    },
+    items: [
+      {
+        itemImage: 'www.url.com/someimg',
+        itemName: 'The Timeless Way of Building',
+        itemAuthor: 'Alexander, Christopher',
+        returnLastDate: {
+          day: 19,
+          month: 8,
+          year: 2020
+        },
+        price: 41.33
+      }
+    ],
+    productSup: false
+  },
+  {
+    orderDate: {
+      day: 4,
+      month: 7,
+      year: 2020
+    },
+    orderCost: 17.22,
+    userName: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    deliveryDate: {
+      day: 7,
+      month: 7,
+      year: 2020
+    },
+    items: [
+      {
+        itemImage: 'www.url.com/someimg',
+        itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        itemAuthor: null,
+        returnLastDate: {
+          day: 5,
+          month: 8,
+          year: 2020
+        },
+        price: 15.98
+      }
+    ],
+    productSup: false
+  },
+  {
+    orderDate: {
+      day: 3,
+      month: 7,
+      year: 2020
+    },
+    orderCost: 138.93,
+    userName: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    deliveryDate: {
+      day: 5,
+      month: 7,
+      year: 2020
+    },
+    items: [
+      {
+        itemImage: 'www.url.com/someimg',
+        itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        itemAuthor: null,
+        returnLastDate: {
+          day: 4,
+          month: 8,
+          year: 2020
+        },
+        price: 94.95
+      },
+      {
+        itemImage: 'www.url.com/someimg',
+        itemName: 'The Are of Sql',
+        itemAuthor: 'Faroult, Stephane',
+        returnLastDate: {
+          day: 4,
+          month: 8,
+          year: 2020
+        },
+        price: 33.99
+      }
+    ],
+    productSup: true
+  }
+];
